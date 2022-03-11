ddb6fe38493d4110c4bc3480808d57d9712b787f
Ahmad <ahmadasadi1381.22@gmail.com>
2022-03-11 15:51:59 +0330

    add search

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ def get_user_list(counts):
     list_user = []
     for counter in range(counts):
         dictionary = {}
-        user_name = input("Enter user.name:")
-        user_age = int(input("Enter user.age:"))
-        dictionary[user_name] = user_age
+        user_name = input("Enter user.name: ")
+        user_age = int(input("Enter user.age: "))
+        dictionary["name"] = user_name
+        dictionary["age"] = user_age
         list_user.append(dictionary)
     return list_user
 
 
 if __name__ == '__main__':
-    user_count = int(input("Enter users counts:"))
+    user_count = int(input("Enter users counts: "))
     user_list = get_user_list(user_count)
-    print(user_list)
+    search = input("Enter name to search: ")
+    result = False
+    for temp in user_list:
+        if temp["name"] == search:
+            print(temp["age"])
+            result = True
+    if not result:
+        print("There is no user with given name!")
