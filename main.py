3a986d5448b69fc875a9a31774ca2fa87036a713
Ahmad <ahmadasadi1381.22@gmail.com>
2022-03-11 15:13:58 +0330

    get users and ages complete

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == '__main__':
-    print()
+    user_list = []
+    user_count = int(input("Enter users counts:"))
+
+    for counter in range(user_count):
+        dictionary = {}
+        user_name = input("Enter user.name:")
+        user_age = int(input("Enter user.age:"))
+        dictionary[user_name] = user_age
+        user_list.append(dictionary)
+
+    print(user_list)
