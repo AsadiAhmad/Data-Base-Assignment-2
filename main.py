d11aea000d80f07f736716cfaedbb1e4185b47e8
Ahmad <ahmadasadi1381.22@gmail.com>
2022-03-11 15:22:24 +0330

    create get_user_list function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-if __name__ == '__main__':
-    user_list = []
-    user_count = int(input("Enter users counts:"))
-
-    for counter in range(user_count):
+def get_user_list(counts):
+    list_user = []
+    for counter in range(counts):
         dictionary = {}
         user_name = input("Enter user.name:")
         user_age = int(input("Enter user.age:"))
         dictionary[user_name] = user_age
-        user_list.append(dictionary)
+        list_user.append(dictionary)
+    return list_user
 
+
+if __name__ == '__main__':
+    user_count = int(input("Enter users counts:"))
+    user_list = get_user_list(user_count)
     print(user_list)
